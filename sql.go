c71bf33a0f2c768446082a1cdc32d92e1069cc10
Georg vom Endt <gve@3yourmind.com>
2020-08-31 11:36:12 +0200

    add String repr of row

--- sql.go
+++ sql.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -27,6 +28,34 @@ type BatteryDataRow struct {
 	VoltageNow       int64
 	Charging         int64
 	Timestamp        int64
+}
+
+func (bdw *BatteryDataRow) String() string {
+	fString := `
+        Timestamp: %10d
+            Power: %10.1f W
+       CurrentNow: %10.1f mA
+       VoltageNow: %10.1f mV
+         Charging: %10b
+        ChargeNow: %10.1f mAh
+       ChargeFull: %10.1f mAh
+ ChargeFullDesign: %10.1f mAh
+ CapacityPermille: %10d ‰
+CapacityDegration: %10d ‰
+`
+	return fmt.Sprintf(
+		fString,
+		bdw.Timestamp,
+		float64(bdw.VoltageNow/1000000*bdw.CurrentNow/1000000),
+		float64(bdw.CurrentNow/1000),
+		float64(bdw.VoltageNow/1000),
+		bdw.Charging,
+		float64(bdw.ChargeNow/1000),
+		float64(bdw.ChargeFull/1000),
+		float64(bdw.ChargeFullDesign/1000),
+		1000.0*bdw.ChargeNow/bdw.ChargeFull,
+		-1000.0*bdw.ChargeFull/bdw.ChargeFullDesign+1000,
+	)
 }
 
 func CreateDatabaseAndTables(db *sql.DB) {
